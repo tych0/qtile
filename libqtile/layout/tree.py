tych0/qtile

commit 9e4d806501c6491e9ec03eba8108a94845433075

replaced click name with button_press to be more consistent with XEvent

libqtile/layout/tree.py
@@ -33,7 +33,7 @@ class TreeNode(object):
         self._children_stop = top
         return top
 
-    def click(self, x, y):
+    def button_press(self, x, y):
         """Returns self or sibling which got the click"""
         if y >= self._children_stop or y < self._children_start:
             return
@@ -190,7 +190,7 @@ class Window(TreeNode):
             return super(Window, self).draw(layout, top, level + 1)
         return top
 
-    def click(self, x, y):
+    def button_press(self, x, y):
         """Returns self if clicked on title else returns sibling"""
         if y >= self._title_start and y < self._children_start:
             return self

libqtile/bar.py
@@ -204,7 +204,7 @@ class Bar(Gap):
     def handle_ButtonPress(self, e):
         for i in self.widgets:
             if e.event_x < i.offset + i.width:
-                i.click(e.event_x - i.offset, e.event_y, e.detail)
+                i.button_press(e.event_x - i.offset, e.event_y, e.detail)
                 break
 
     def widget_grab_keyboard(self, widget):
@@ -242,9 +242,9 @@ class Bar(Gap):
             window=self.window.window.wid
         )
 
-    def cmd_fake_click(self, screen, position, x, y, button=1):
+    def cmd_fake_button_press(self, screen, position, x, y, button=1):
         """
-            Fake a mouse-click on the bar. Co-ordinates are relative
+            Fake a mouse-button-press on the bar. Co-ordinates are relative
             to the top-left corner of the bar.
 
             :screen The integer screen offset

libqtile/widget/base.py
@@ -77,7 +77,7 @@ class _Widget(command.CommandObject):
             width=self.width,
         )
 
-    def click(self, x, y, button):
+    def button_press(self, x, y, button):
         pass
 
     def get(self, q, name):

libqtile/widget/canto.py
@@ -41,7 +41,7 @@ class Canto(base._TextBox):
                 number=check_output(["canto", "-n", feed])[:-1]
             ) for feed in self.feeds])
 
-    def click(self, x, y, button):
+    def button_press(self, x, y, button):
         self.update()
 
     def update(self):

libqtile/widget/crashme.py
@@ -28,7 +28,7 @@ class _CrashMe(base._TextBox):
             self.text, self.foreground, self.font, self.fontsize,
             markup=True)
 
-    def click(self, x, y, button):
+    def button_press(self, x, y, button):
         if button == 1:
             1 / 0
         elif button == 3:

libqtile/widget/currentlayout.py
@@ -26,7 +26,7 @@ class CurrentLayout(base._TextBox):
                 self.bar.draw()
         hook.subscribe.layout_change(hook_response)
 
-    def click(self, x, y, button):
+    def button_press(self, x, y, button):
         if button == 1:
             self.qtile.cmd_nextlayout()
         elif button == 2:

libqtile/widget/groupbox.py
@@ -75,7 +75,7 @@ class AGroupBox(_GroupBase):
         ("padding", 5, "Padding inside the box")
     )
 
-    def click(self, x, y, button):
+    def button_press(self, x, y, button):
         self.bar.screen.group.cmd_nextgroup()
 
     def calculate_width(self):
@@ -124,7 +124,7 @@ class GroupBox(_GroupBase):
     def __init__(self, **config):
         base._Widget.__init__(self, bar.CALCULATED, **config)
 
-    def click(self, x, y, button):
+    def button_press(self, x, y, button):
         group = None
         curGroup = self.qtile.currentGroup
         if button == 5:

libqtile/widget/mpdwidget.py
@@ -159,7 +159,7 @@ class Mpd(base._TextBox):
 
         return True
 
-    def click(self, x, y, button):
+    def button_press(self, x, y, button):
         if not self.connect(True):
             return False
         try:

commit 1462429a67f98cff6791111c5da1fbcd9954ca8a

Fix some rename typos
9e4d806501c6491e9ec03eba8108a94845433075 introduced some inconsistencies; this
just synchronizes things.

libqtile/layout/tree.py
@@ -38,7 +38,7 @@ class TreeNode(object):
         if y >= self._children_stop or y < self._children_start:
             return
         for i in self.children:
-            res = i.click(x, y)
+            res = i.button_press(x, y)
             if res is not None:
                 return res
 
@@ -194,7 +194,7 @@ class Window(TreeNode):
         """Returns self if clicked on title else returns sibling"""
         if y >= self._title_start and y < self._children_start:
             return self
-        return super(Window, self).click(x, y)
+        return super(Window, self).button_press(x, y)
 
     def remove(self):
         self.parent.children.remove(self)
@@ -312,7 +312,7 @@ class TreeTab(SingleWindow):
         self._drawer.draw(0, self.panel_width)
 
     def _panel_ButtonPress(self, event):
-        node = self._tree.click(event.event_x, event.event_y)
+        node = self._tree.button_press(event.event_x, event.event_y)
         if node:
             self.group.focus(node.window, False)
 