a8b10267999186cfa5e14df95f159205ce7932a7
Tycho Andersen <tycho@tycho.ws>
2013-10-02 13:35:57 -0500

    Warn people when missing widget depenencies
    Based on some discussion in IRC, getting a NameError is confusing for some
    users, so we do something a little nicer.

--- libqtile/widget/__init__.py
+++ libqtile/widget/__init__.py
@@ -1,3 +1,6 @@
+# This is mostly just a hack to avoid stuff like:
+#   from libqtile.widget.prompt import Prompt
+
 from backlight import Backlight
 from battery import Battery, BatteryIcon
 from clock import Clock
@@ -6,12 +9,14 @@ from graph import CPUGraph, MemoryGraph, SwapGraph, NetGraph, HDDGraph, HDDBusyG
 from groupbox import AGroupBox, GroupBox
 from maildir import Maildir
 from notify import Notify
+from pacman import Pacman
 from prompt import Prompt
 from sensors import ThermalSensor
 from sep import Sep
 from she import She
 from spacer import Spacer
 from systray import Systray
+from tasklist import TaskList
 from textbox import TextBox
 from volume import Volume
 from windowname import WindowName
@@ -20,37 +25,25 @@ from keyboardlayout import KeyboardLayout
 from df import DF
 from image import Image
 
-from tasklist import TaskList
 
-try:
-    from canto import Canto
-except ImportError:
-    pass
+# We use lazy_import to warn people when they are missing dependencies. If your
+# widget has extra dependencies that are not listed in
+# /docs/manual/install/source.rst then you should use this wrapper.
+def lazy_import(module, widget):
+    def fake(*args, **kwargs):
+        try:
+            m = __import__(module, fromlist=module)
+            cls = getattr(m, widget)
+            return cls(*args, **kwargs)
+        except ImportError as e:
+            return TextBox(e.message)
+    return fake
 
-try:
-    from mpriswidget import Mpris
-except ImportError:
-    pass
 
-try:
-    from mpdwidget import Mpd
-except ImportError:
-    pass
-
-try:
-    from yahoo_weather import YahooWeather
-    from bitcoin_ticker import BitcoinTicker
-except ImportError:
-    # Requires Python >= 2.6 or simplejson
-    pass
-from pacman import Pacman
-from sensors import ThermalSensor
-try:
-    from wlan import Wlan
-except ImportError:
-    # Requires python-wifi
-    pass
-try:
-    from google_calendar import GoogleCalendar
-except ImportError:
-    pass
+Canto = lazy_import("canto", "Canto")
+Mpris = lazy_import("mpriswidget", "Mpris")
+Mpd = lazy_import("mpdwidget", "Mpd")
+YahooWeather = lazy_import("yahoo_weather", "YahooWeather")
+BitcoinTicker = lazy_import("bitcoin_ticker", "BitcoinTicker")
+Wlan = lazy_import("wlan", "Wlan")
+GoogleCalendar = lazy_import("google_calendar", "GoogleCalendar")
