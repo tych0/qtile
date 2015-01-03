711ef3b816ef512c4caabc8d257f5ab685a511d1
Tycho Andersen <tycho@tycho.ws>
2015-01-02 19:28:24 -0600

    Release 0.9.0

--- docs/conf.py
+++ docs/conf.py
@@ -43,14 +43,14 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Qtile'
-copyright = u'2008-2014, Aldo Cortesi and contributers'
+copyright = u'2008-2015, Aldo Cortesi and contributers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = '0.8.0'
+version = '0.9.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
